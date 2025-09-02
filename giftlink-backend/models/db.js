e8904326b7fdb1b2950969c890a7e9870ac99159
rfaze <randy.ssve@gmail.com>
2025-09-02 17:18:37 +0200

    implementing searchRoutes

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -15,14 +15,9 @@ async function connectToDatabase() {
 
     const client = new MongoClient(url);      
 
-    // Task 1: Connect to MongoDB
     await client.connect();
-    console.log("Connected successfully to server");
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
     dbInstance = client.db(dbName);
 
-    // Task 3: Return database instance
     return dbInstance;
 }
 
