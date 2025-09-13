9443a50478aeeed31b2152e8ba38b55620e38e02
rfaze <randy.ssve@gmail.com>
2025-09-13 16:57:18 +0200

    backend in register component

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 
 import './RegisterPage.css';
 
@@ -9,8 +13,40 @@ function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const [showerr, setShowerr] = useState('');
+    const navigate = useNavigate();
+        const { setIsLoggedIn } = useAppContext();
+
+
     const handleRegister = async () => {
-        console.log("Register invoked")
+        const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
+            })
+        });
+
+        const json = await response.json();
+        console.log('json data', json);
+        console.log('er', json.error);
+
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', firstName);
+            sessionStorage.setItem('email', json.email);
+
+            setIsLoggedIn(true);
+            navigate('/app');
+        }
+        if (json.error) {
+            setShowerr(json.error);
+        }
     }
 
     return (
@@ -52,6 +88,7 @@ function RegisterPage() {
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
+                            <div className="text-danger">{showerr}</div>
                         </div>
                         <div className="mb-4">
                             <label htmlFor="password" className="form-label">Password</label>
